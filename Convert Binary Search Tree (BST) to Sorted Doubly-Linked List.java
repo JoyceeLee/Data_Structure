dc9144eedd50b5be26878d05aff8c613e6b87a64
YangLi <liang.900503@gmail.com>
2014-07-17 21:37:44 -0700

    Update Convert Binary Search Tree (BST) to Sorted Doubly-Linked List.java

--- Convert Binary Search Tree (BST) to Sorted Doubly-Linked List.java
+++ Convert Binary Search Tree (BST) to Sorted Doubly-Linked List.java
@@ -3,7 +3,7 @@
 * Think of the left and right pointers as synonymous to the previous and next pointers in a doubly-linked list.
 */
 
-
+// Solution 1 : In-order
 public class Solution {
     public TreeNode BST2DLL(TreeNode root) {
         TreeNode pre = null;
@@ -27,5 +27,31 @@ public class Solution {
         cur.right = head;
         pre = cur;
         helper(right, pre, head);
+    }
+}
+
+
+// Solution 2 : Divided and Conquer
+public class Solution {
+    public void join(TreeNode a, TreeNode b) {
+        a.right = b;
+        b.left = a;
+    }
+    public TreeNode connect(TreeNode lista, TreeNode listb) {
+        TreeNode alast = lista.left;
+        TreeNode blast = listb.left;
+        join(alast, listb);
+        join(blast, lista);
+        return lista;
+    }
+    public TreeNode BST2DLL(TreeNode root) {
+        if(root==null) return;
+        TreeNode alist = BST2DLL(root.left);
+        TreeNode blist = BST2DLL(root.right);
+        root.left = root;
+        root.right = root;
+        alist = connect(alist, root);
+        alist = connect(alist, blist);
+        reurn alist;
     }
 }
